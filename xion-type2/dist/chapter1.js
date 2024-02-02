22cd9ed670865deee1f324566ba90222af2ecf29
hcche <hcche@plateer.com>
2024-02-02 11:37:34 +0900

    폴더 구조 변경

---
+++ xion-type2/dist/chapter1.js
@@ -0,0 +1,17 @@
+// number
+let num1 = 123; // : 은 typeAnontation 이라고 한다.
+//num1 = null;  // "strictNullChecks": false 때문에 가능
+// string
+let str1 = "Hello";
+let str2 = `Hello ${num1}`;
+// boolean
+let bool1 = true;
+let bool2 = false;
+let null1 = null;
+// undefined
+let undefined1 = undefined;
+// 리터럴 타입
+// 리터럴 -> 값
+let numA = 10;
+let strA = "Hello";
+export {};
---
+++ xion-type2/src/chapter1.ts
@@ -0,0 +1,21 @@
+// number
+let num1 : number = 123;  // : 은 typeAnontation 이라고 한다.
+//num1 = null;  // "strictNullChecks": false 때문에 가능
+
+// string
+let str1 : string = "Hello";
+let str2 : string = `Hello ${num1}`;
+
+// boolean
+let bool1 : boolean = true;
+let bool2 : boolean = false;
+
+let null1 : null = null;
+
+// undefined
+let undefined1 : undefined = undefined;
+
+// 리터럴 타입
+// 리터럴 -> 값
+let numA : 10 = 10;
+let strA : "Hello" = "Hello";

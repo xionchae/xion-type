40b9509b37cc235ee8428e8fd90166832981406e
hcche <hcche@plateer.com>
2024-02-05 11:39:49 +0900

    섹션2 - 타입 별칭과 인덱스 시그니쳐

--- xion-type2/dist/chapter2.js
+++ xion-type2/dist/chapter2.js
@@ -5,4 +5,16 @@ let boolArr = [true, false];
 let boolArr2 = [true, false]; // 제네릭 타입(문법)
 // 배열에 들어가는 요소들의 타입이 다양할 경우
 let multiArr = [1, "Hello", true]; // (number | string | boolean)[]
+// 다차원 배열
+let doubleArr = [[1, 2], [3, 4]];
+// 튜플 (javascript에는 없고 typescript에만 있는 타입)
+// 길이와 타입이 고정된 배열
+let tup1 = [1, 2];
+// tub1 = [1,2,3]  // Error
+const users = [
+    ["xion", 1],
+    ["type", 2],
+    ["xion-type", 3],
+    // [5,"xion-type2",4]  // 이렇게 넣을 수도 있기 때문에 튜블 사용
+];
 export {};
---
+++ xion-type2/src/chapter4.ts
@@ -0,0 +1,33 @@
+// 타입 별칭
+// 타입 별칭은 특정 타입이나 인터페이스를 참조할 수 있는 타입 변수를 의미한다.
+// 타입 별칭은 컴파일 시에는 사라지기 때문에 런타임에 영향을 주지 않는다. dist 폴더에는 타입 별칭이 사라진 것을 확인할 수 있다.
+type User = {
+  id:number;
+  name:string;
+  nickname:string;
+  birth:string;
+  bio:string;
+  location:string;
+}
+
+let user1 : User ={
+  id: 1,
+  name:"xion",
+  nickname:"xion",
+  birth:"1999-01-01",
+  bio:"hello world",
+  location:"seoul"
+}
+
+// 인텍스 시그니처
+type CountryNumberCodes = {
+  [key:string]:number;
+  // Korea에 string을 할당할 수는 없다.
+  Korea:number;   // 빈객체를 만들면 에러가 발생하게 하기 위해서 꼭 Korea가 들어가야 에러가 안나게 설정한다.
+}
+
+let contryCodes :CountryNumberCodes = {
+  Korea:80,
+  USA:23,
+  China:34
+}
